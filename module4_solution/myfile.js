7b674bd2aee4657d2bc95e774d0fa5b65a6ccdfb
MuqadasUrooj <104297720+MuqadasUrooj@users.noreply.github.com>
2022-12-17 03:54:46 -0800

    Update myfile.js

--- module4_solution/myfile.js
+++ module4_solution/myfile.js
@@ -13,10 +13,10 @@ for (var i=0;i<names.length;i++)
 {
     if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j')
     {
-     console.log("Goodbye"+ names[i])
+     console.log("Goodbye "+ names[i])
 }
 else {
-     console.log("Hello"+  names[i])
+     console.log("Helle "  +  names[i])
 
 }
 }
